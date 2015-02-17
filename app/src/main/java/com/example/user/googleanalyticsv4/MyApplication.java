a6bcff6d24b1987e564bc509989630f862b953e1
MSSSoftrodigy <mss.softprodigy@gmail.com>
2015-02-17 15:37:11 +0530

    GoogleAnalyticsV4
    This is tutorial for new version of google analytics v4

---
+++ app/src/main/java/com/example/user/googleanalyticsv4/MyApplication.java
@@ -0,0 +1,47 @@
+package com.example.user.googleanalyticsv4;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+import java.util.HashMap;
+
+/**
+ * Created by user on 2/17/2015.
+ */
+public class MyApplication extends Application {
+
+    // The following line should be changed to include the correct property id.
+    private static final String PROPERTY_ID = "UA-46714754-3";
+
+    //Logging TAG
+    private static final String TAG = "MyApp";
+
+    public static int GENERAL_TRACKER = 0;
+
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+        GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg: roll-up tracking.
+        ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from a company.
+    }
+
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+    public MyApplication() {
+        super();
+    }
+
+    synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker(R.xml.app_tracker)
+                    : (trackerId == TrackerName.GLOBAL_TRACKER) ? analytics.newTracker(PROPERTY_ID)
+                    : analytics.newTracker(R.xml.app_tracker);
+            mTrackers.put(trackerId, t);
+
+        }
+        return mTrackers.get(trackerId);
+    }
+}
